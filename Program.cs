fe8bac4a1724d36f59be62d788706428c82c364b
Nikita Tsygulev <hekitos007@mail.ru>
2023-04-18 10:50:23 +0300

    Сделал задание

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string outt = Magic("Hello world",2);
+Console.WriteLine(outt);
+
+
+string Magic(string inpt_str, int rule)
+{
+    string magic_string = string.Empty;
+    if (rule > 255) { rule -= 255; }
+    for (int i = 0; i < inpt_str.Length; i++)
+    {
+        magic_string += (char)((int)inpt_str[i] + rule);
+    }
+    return magic_string;
+}
